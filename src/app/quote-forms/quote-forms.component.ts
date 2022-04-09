96b2a0f7b7e214d2aa90ca0d76c71213fa6162be
Karenkoech <karenkoech3@gmail.com>
2022-04-09 15:28:30 -0400

    create form component and class

---
+++ src/app/quote-forms/quote-forms.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from './quote'
+@Component({
+  selector: 'app-quote-forms',
+  templateUrl: './quote-forms.component.html',
+  styleUrls: ['./quote-forms.component.css']
+})
+export class QuoteFormsComponent implements OnInit {
+
+  userModel=new User
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
